a56b23e91ff8380e58767bfc09583d01b241766e
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-13 11:40:58 -0400

    The FOIL-UI Components works in vue.js.

--- Experiments-with-Vue.js/experiment-with-foil-ui/src/components/HelloWorld.vue
+++ Experiments-with-Vue.js/experiment-with-foil-ui/src/components/HelloWorld.vue
@@ -1,31 +1,43 @@
 <template>
-  <div class="hello">
+  <div class="FOIL-Layout color-is-black">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+   </div>
+
+   <div class="FOIL-Layout color-is-black">
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" class="FOIL-button" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" class="FOIL-button" rel="noopener">eslint</a></li>
     </ul>
+    </div> 
+
+    
+   <div class="FOIL-Layout color-is-black">
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a href="https://vuejs.org" target="_blank" class="FOIL-button" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" class="FOIL-button" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" class="FOIL-button"  rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" class="FOIL-button" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" class="FOIL-button" rel="noopener">News</a></li>
     </ul>
+    </div> 
+
+
+    
+   <div class="FOIL-Layout color-is-black">
     <h3>Ecosystem</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://router.vuejs.org" target="_blank" class="FOIL-button" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" class="FOIL-button" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" class="FOIL-button" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" class="FOIL-button" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" class="FOIL-button" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
 </template>
@@ -56,3 +68,5 @@ a {
   color: #42b983;
 }
 </style>
+
+
